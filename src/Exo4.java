b11aca216f22283e0c9987b54b2a4021335d59d9
Kaiserben67 <atton10@live.fr>
2024-10-15 14:36:36 +0200

    bêta test

---
+++ src/Exo4.java
@@ -0,0 +1,12 @@
+public class Exo4 {
+
+    /*  EXERCICE 4 : Faire le compte à rebour à partir de 10, affiche successivment décompte une fois à 0, afficher bonne année ! */
+
+    public static void main(String[] args) throws Exception {
+for (int i=10 ; i>=0 ; i--){
+           /* Afficher le résultat */
+        System.out.println(i);
+    }
+       System.out.println("Bonne année !");  
+}
+}
