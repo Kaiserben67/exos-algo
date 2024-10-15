761f6688ef988abad6de7b69df768ac9c5b370fb
Kaiserben67 <atton10@live.fr>
2024-10-15 14:32:08 +0200

    vbêta

---
+++ src/exo5.java
@@ -0,0 +1,13 @@
+public class exo5 {
+    /*  EXERCICE 5 : Créer un algo qui vérifie entre chaque nombres de 1 à 20 , si ils sont paires ou impaires */
+
+    public static void main(String[] args) throws Exception {
+        int nbr=20;        
+       
+        if (nbr%2==0)
+        System.out.println("Le nombre est pair.");
+        else
+        System.out.println("Le nombre est impair.");
+        /* Afficher le résultat */
+    }
+}
